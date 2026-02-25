006b82deb5b2f45ff5c5f90eb879f72ce6ffc1ae
aswinamahendra <igustiaswinam@gmail.com>
2026-02-25 09:42:13 +0700

    Added Google Tag Script

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 
-const GTAG_ID = 'GTM-KQRNFRNW';
+const NEXT_PUBLIC_GTAG_ID = 'AW-17060346146';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      window.gtag?.('config', GTAG_ID, { page_path: url });
+      window.gtag?.('config', NEXT_PUBLIC_GTAG_ID, { page_path: url });
     };
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => router.events.off('routeChangeComplete', handleRouteChange);
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GTAG_ID}`}
       />
       <Script
         id="gtag-init"
@@ -29,7 +29,20 @@ function MyApp({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GTAG_ID}');
+            gtag('config', '${NEXT_PUBLIC_GTAG_ID}');
+          `,
+        }}
+      />
+      <Script
+        id="gtag-conversion"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            gtag('event', 'conversion', {
+              'send_to': 'AW-17060346146/58SaCMXCjcUaEKLygMc_',
+              'value': 1.0,
+              'currency': 'IDR'
+            });
           `,
         }}
       />
